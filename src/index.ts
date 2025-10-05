65808ce92baa6c124d1eeb8237431a5af7f9a006
Hardik Yadav <hardikyadav1182005@gmail.com>
2025-10-06 01:51:38 +0530

    cors_fix_second

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ import cors from "cors"
 const app = express();
 
 app.use(express.json());
-app.use("*", cors({
+
+app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
